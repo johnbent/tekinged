87d6aa030e87e1743bbf203761a48655c2504607
John Bent <john.bent@SGS-MAC-02.local>
2017-06-25 21:22:22 -0600

    instead of capturing the output, now it does output direct to file, so it can give progress info to stdout

--- scripts/mk_xml.py
+++ scripts/mk_xml.py
@@ -11,44 +11,45 @@ def query(q,c):
   c.execute(q)
   return c.fetchall()
 
-def print_footer():
-  print '</d:dictionary>'
+def print_footer(xml):
+  xml.write( '</d:dictionary>' )
 
-def print_header():
-  print '''<?xml version="1.0" encoding="UTF-8"?>
+def print_header(xml):
+  xml.write( '''<?xml version="1.0" encoding="UTF-8"?>
 <!--
-  This is a sample dictionary source file.
+  This is the Palauan dictionary source file as produced using the tekinged.com database.
   It can be built using Dictionary Development Kit.
 -->
-<d:dictionary xmlns="http://www.w3.org/1999/xhtml" xmlns:d="http://www.apple.com/DTDs/DictionaryService-1.0.rng"> '''
+<d:dictionary xmlns="http://www.w3.org/1999/xhtml" xmlns:d="http://www.apple.com/DTDs/DictionaryService-1.0.rng"> ''')
 
-def add_image(wid,pal):
+def add_image(wid,pal,xml):
   if os.path.isfile('OtherResources/Images/%d.jpg' % wid ):
-    print '''\
+    xml.write( '''\
           <span class="picture">
           <img src="Images/%d.jpg" alt="%s"/>
-          </span>''' % ( wid, pal )
+          </span>''' % ( wid, pal ))
     
 def space(multiplier=1):
   width=5*multiplier
   return '<span style="display:inline-block; width: %dpx;"></span>' % width
 
-def add_branch_words(branches):
+def add_branch_words(branches,xml):
   #print row
   for row in branches: 
-    display_word(row,'b')
+    display_word(row,'b',xml)
 
-def display_word(row,htype):
-  def print_english(eng):
+def display_word(row,htype,xml):
+  def get_eng(eng):
     if eng:
       return eng
     else:
       return ''
 
-  print '\t<div d:priority="2" class="entry">%s<%s style="display: inline">%s</%s> %s <i>%s</i> %s %s' % ( space(), htype, row['pal'], htype, space(), row['pos'], space(), print_english(row['eng']) )
+  xml.write( '\t<div d:priority="2" class="entry">%s<%s style="display: inline">%s</%s> %s <i>%s</i> %s %s' % 
+            ( space(), htype, row['pal'], htype, space(), row['pos'], space(), get_eng(row['eng']) ))
   if row['pdef']:
-    print '\t<span class="pdef" d:priority="2">%s</span>' % row['pdef']
-  print '</div>'
+    xml.write( '\t<span class="pdef" d:priority="2">%s</span>' % row['pdef'])
+  xml.write( '</div>' )
 
 def get_pal(wid,c):
   q='select pal from all_words3 where id=%d' % wid
@@ -63,92 +64,95 @@ def get_regexes(word,branches):
   regex+='0'
   return regex 
 
-def add_proverbs(word,branches,c):
-  add_sentences(word,branches,c,'proverbs','PROVERBS','english','explanation')
+def add_proverbs(word,branches,c,xml):
+  add_sentences(word,branches,c,'proverbs','PROVERBS','english',xml,'explanation')
 
-def add_examples(word,branches,c):
-  add_sentences(word,branches,c,'examples','JOSEPHS DICTIONARY EXAMPLE SENTENCES','english')
+def add_examples(word,branches,c,xml):
+  add_sentences(word,branches,c,'examples','JOSEPHS DICTIONARY EXAMPLE SENTENCES','english',xml)
 
-def add_uploads(word,branches,c):
-  add_sentences(word,branches,c,'upload_sentence','VOLUNTEER UPLOADED EXAMPLE SENTENCES','eng')
+def add_uploads(word,branches,c,xml):
+  add_sentences(word,branches,c,'upload_sentence','VOLUNTEER UPLOADED EXAMPLE SENTENCES','eng',xml)
 
-def add_sentences(word,branches,c,table,label,english,explanation=None):
-  q='select * from %s where %s limit 5' % (table, get_regexes(word,branches))
+def add_sentences(word,branches,c,table,label,english,xml,explanation=None):
+  q='select * from %s where %s limit 7' % (table, get_regexes(word,branches))
   examples = query(q,c)
   if examples:
-    print '<span class="column">'
-    print label 
+    xml.write( '<span class="column">')
+    xml.write( '%s' % label )
     for example in examples:
       if explanation:
         explanation_text = '[%s]' % example[explanation]
       else:
         explanation_text = ''
-      print '<div class="entry"><i>%s</i> %s %s</div>' % ( example['palauan'], example[english], explanation_text )
-    print '</span>'
-  #print branches
+      xml.write( '<div class="entry"><i>%s</i> %s %s</div>' % ( example['palauan'], example[english], explanation_text ) )
+    xml.write( '</span>' )
 
-def add_synonyms(row,c):
+def add_synonyms(row,c,xml):
   q='select word as b from synonyms where grouping in (select grouping from synonyms where word=%d) and word !=%d' % (row['id'],row['id'])
-  add_extras(q,c,'SYNONYMS')
+  add_extras(q,c,'SYNONYMS',xml)
 
-def add_links(row,c):
+def add_links(row,c,xml):
   q='select b from cf where a=%d union all select a from cf where b=%d' % (row['id'],row['id'])
-  add_extras(q,c,'SEE ALSO')
+  add_extras(q,c,'SEE ALSO',xml)
 
-def add_extras(q,c,label):
+def add_extras(q,c,label,xml):
   extras = query(q,c)
   if extras:
-    print '<div class="extra" d:priority="2"><h1 style="display: inline">%s:</h1>' % label
+    xml.write( '<div class="extra" d:priority="2"><h1 style="display: inline">%s:</h1>' % label )
     for extra in extras:
-      print get_pal(extra['b'],c)
-    print '</div>'
+      xml.write( get_pal(extra['b'],c) )
+    xml.write( '</div>' )
 
-def print_word(row,c):
+def print_word(row,c,xml):
   # get the branch words
   branches = query('select id,pal,eng,pdef,pos from all_words3 where stem=%d and stem!=id order by pal' % row['id'], c)
 
   # main header for each word
-  print '<d:entry id="%s_%d" d:title="%s">' % ( row['pal'], row['id'], row['pal'] )
+  xml.write( '<d:entry id="%s_%d" d:title="%s">' % ( row['pal'], row['id'], row['pal'] ) )
 
   # index entries for the word and its branches
-  print '\t<d:index d:value="%s"/>' % row['pal']
+  xml.write( '\t<d:index d:value="%s"/>' % row['pal'] )
   for branch in branches:
-    print '\t<d:index d:value="%s"/>' % branch['pal']
+    xml.write( '\t<d:index d:value="%s"/>' % branch['pal'] )
 
   # show a header 
-  print '<div d:priority="2"><h1>%s</h1></div>' % row['pal']
+  xml.write( '<div d:priority="2"><h1>%s</h1></div>' % row['pal'] )
 
   # now show the word
-  display_word(row,'b')
+  display_word(row,'b',xml)
 
   # now add the branches
-  add_branch_words(branches)
+  add_branch_words(branches,xml)
 
   # now add extras 
-  add_links(row,c)
-  add_synonyms(row,c)
-  add_examples(row,branches,c)
-  add_uploads(row,branches,c)
-  add_proverbs(row,branches,c)
+  add_links(row,c,xml)
+  add_synonyms(row,c,xml)
+  add_examples(row,branches,c,xml)
+  add_uploads(row,branches,c,xml)
+  add_proverbs(row,branches,c,xml)
 
   # now add the picture
-  add_image(row['id'],row['pal'])
+  add_image(row['id'],row['pal'],xml)
 
   # end the word
-  print '</d:entry>'
+  xml.write( '</d:entry>' )
 
 
 def main():
   (db,c) = belau.connect()
 
-  print_header()
+  xml = open('Palauan.xml', 'w')
+  print_header(xml)
 
-  q = "select id,pal,eng,pdef,pos,oword,origin from all_words3 where id=stem and pal in ('rubak', 'meas', 'mengelebed', 'chelebed','omar')";
-  for row in query(q,c):
-    print_word(row,c)
+  q = "select id,pal,eng,pdef,pos,oword,origin from all_words3 where id=stem limit 10"; 
+  words = query(q,c)
+  for idx,row in enumerate(words):
+    print '%5d/%d %s' % (idx, len(words), row['pal'])
+    print_word(row,c,xml)
 
-  print_footer()
+  print_footer(xml)
 
+  xml.close()
   db.commit()
   c.close()
   db.close()
